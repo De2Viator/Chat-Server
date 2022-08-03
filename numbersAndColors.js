c453cabe782c4a88c2d15152f6aa05e6dc6d14ce
De2Viator <troshenkoyura@gmail.com>
2022-08-03 18:23:22 +0300

    1.1

---
+++ numbersAndColors.js
@@ -0,0 +1,36 @@
+import colors from 'colors'
+const processes = process.argv.slice(2);
+
+const isPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++)
+        if (n % i == 0) {
+            return false;
+        }
+    return true;
+}
+
+for (let i = 0; i < 3; i++) {
+    const process = +processes[i];
+    if (!isNaN(process)) {
+        if (isPrime(process)) {
+            switch (i) {
+                case 0: {
+                    console.log(colors.green(process));
+                    break;
+                }
+                case 1: {
+                    console.log(colors.yellow(process));
+                    break;
+                }
+                case 2: {
+                    console.log(colors.red(process));
+                    break;
+                }
+            }
+        } else {
+            console.log(colors.red('Число не является простым'))
+        }
+    } else {
+        console.log(colors.red('Аргумент не является числом!!'))
+    }
+}
