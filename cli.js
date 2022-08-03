c6ec5c5f1d67f4cb4b47480a60403ce3bc31365f
De2Viator <troshenkoyura@gmail.com>
2022-08-04 00:20:35 +0300

    add cli

---
+++ cli.js
@@ -0,0 +1,23 @@
+import { Command } from 'commander';
+import ansi from 'ansi';
+const program = new Command();
+
+program
+    .name('Число для вывода')
+    .description('CLI для вывода наибольшего числа')
+    .version('1.0.0');
+
+program.command('sort')
+    .description('В консоль отобразится наибольшее число')
+    .argument('<[numbers]>', 'массив для ввода')
+    .action((str) => {
+        const numbers = JSON.parse(str);
+        console.log(numbers)
+        const sortedNumbers = numbers.sort();
+        const num = sortedNumbers.at(-1);
+        const cursor = ansi(process.stdout)
+        cursor.brightCyan().bg.white().write(`${num}`).bg.reset();
+        cursor.reset();
+    });
+
+program.parse();
