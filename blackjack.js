e400a8a911508d118e848e3abf351f9f85fb80fd
De2Viator <troshenkoyura@gmail.com>
2022-08-04 23:44:13 +0300

    add analysis

--- blackjack.js
+++ blackjack.js
@@ -1,22 +1,34 @@
 import inquirer from "inquirer";
 import colors from 'colors';
+import fs from 'fs';
+
+const args = process.argv.slice(2);
+const path = args[0];
 
 class Player {
     constructor(name) {
         this.name = name;
-     }
+    }
+
     points = 0;
     isEndGame = false;
     getCard() {
-        this.points += Player.getRandom(0, 12)
+        const points = Player.getRandom(0, 12);
+        this.points += points;
+        fs.writeFile(path, `${this.name} взял ${points} \n`,{flag:'a',encoding:'utf-8'},(err) => {
+        })
     }
 
     drop() {
         if(!this.isEndGame) {
             if (this.points > 21 || this.points < 21) {
                 console.log(`${this.name} проиграл`)
+                fs.writeFile(path, `${this.name} проиграл \n`,{flag:'a',encoding:'utf-8'}, (err) => {
+                })
             } else {
-                console.log(`${this.name} победил `)
+                console.log(`${this.name} победил`)
+                fs.writeFile(path, `${this.name} победил \n`, {flag:'a',encoding:'utf-8'},(err) => {
+                })
             }
             this.isEndGame = true;
         }
@@ -31,9 +43,11 @@ const player = new Player('Игрок');
 const enemy = new Player('Противник');
 
 const enemyPart = () => {
-    const choose = Player.getRandom(-2,2);
-    if( choose > 0) enemy.getCard();
-    else enemy.drop(enemy.points);
+    if(!enemy.isEndGame) {
+        const choose = Player.getRandom(-2,2);
+        if( choose > 0) enemy.getCard();
+        else enemy.drop(enemy.points);
+    }
 }
 
 const ask = () => {
@@ -52,10 +66,22 @@ const ask = () => {
             ask();
         } else {
             player.drop(player.points);
+            logResult();
         }
     })
 }
 
+const logResult = () => {
+    if(player.isEndGame && enemy.isEndGame) {
+        fs.writeFile('./logs.txt',`Очки игрока:${player.points} 
+Очки противника:${enemy.points}`,
+        {flag:'a',encoding:'utf-8'},(err) => {
+
+        })
+    }
+}
+
 ask();
+
 
 
