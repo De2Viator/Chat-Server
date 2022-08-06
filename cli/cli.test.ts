5ffe4827dbb4a9317c55b0c0e089dad097914318
De2Viator <troshenkoyura@gmail.com>
2022-08-06 15:01:09 +0300

    reset master branch

--- cli/cli.test.ts
+++
@@ -1,9 +0,0 @@
-describe('test cli', () => {
-    it('Test number',() => {
-        const testString = '[2,3,4]'
-        const numbers = JSON.parse(testString);
-        const sortedNumbers: number[] = numbers.sort();
-        const num: number | undefined = sortedNumbers.at(-1);
-        expect(num).toBe(4)
-    })
-})
