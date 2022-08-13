185d2d59bb2db78a657927d3be0e745afe9e80e9
De2Viator <troshenkoyura@gmail.com>
2022-08-13 18:21:32 +0300

    add json-server

--- endpoints/server.ts
+++ endpoints/server.ts
@@ -1,32 +1,87 @@
 import express from 'express';
+import fetch from 'isomorphic-fetch';
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
 app
-.get('/chats',(req,res) => {
-    res.send('chat was getted')
+.get('/chats',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users').then(resp => resp.json());
+    res.send(info)
 })
-.post('/chats', (req,res) => {
-    res.send('chat was posted')
+.post('/chats',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    })
+    res.send(info)
 })
-.put('/chats/:id',(req,res) => {
-    res.send('chat was updated')
+.put('/chats/:id',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/'+ req.params.id, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    })
+    res.send(info)
 })
-.delete('/chats/:id',(req,res) => {
-    res.send('chat was deleted')
+.delete('/chats/:id',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/' + req.params.id, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    })
+    res.send(info)
 })
 
 app
-.get('/messages',(req,res) => {
-    res.send('chat was getted')
+.get('/messages/:chatId',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/'+req.params.chatId).then(resp => resp.json());
+    res.send(info)
 })
-.post('/messages/:chatId', (req,res) => {
-    res.send('chat was posted')
+.post('/messages/:chatId',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/'+req.params.chatId+'/chats', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    }).then(resp => resp.json());
+    res.send(info)
 })
-.put('/messages/:id',(req,res) => {
-    res.send('chat was updated')
+.put('/messages/:id',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/'+ req.params.id, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    })
+    res.send(info)
 })
-.delete('/messages/:id',(req,res) => {
-    res.send('chat was deleted')
+.delete('/messages/:id',async (req,res) => {
+    const info = await fetch('http://localhost:3000/users/' + req.params.id, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    })
+    res.send(info)
 })
 
 app.use(express.json());
@@ -34,6 +89,6 @@ app.use(express.urlencoded({
     extended:true
 }));
 
-app.listen(3000, () => {
-  console.log("Listen port 3000");
+app.listen(3030, () => {
+  console.log("Listen port 3030");
 });
